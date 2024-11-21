49b076dbbb45b4a78e4561285e73ebf4b143476a
vijaykumar <vijaykumarreddy200@gmail.com>
2024-11-21 18:12:04 +0530

    changes IP

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["3.83.104.228"]/' /home/ubuntu/aws_code_pipeline/mysite/settings.py
+sed -i 's/\[]/\["54.196.157.166"]/' /home/ubuntu/aws_code_pipeline/mysite/settings.py
 
 python manage.py migrate 
 # python manage.py makemigrations     
