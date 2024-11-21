736ea7ba6239be5c075c2c7d3e153bc5c3013a5b
vijaykumar <vijaykumarreddy200@gmail.com>
2024-11-21 19:59:38 +0530

    project change6

--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 sudo chown -R ubuntu:ubuntu ~/aws_code_pipeline
-virtualenv /home/ubuntu/aws_code_pipeline/venv
-source /home/ubuntu/aws_code_pipeline/venv/bin/activate
+virtualenv /home/ubuntu/aws_code_pipeline/env
+source /home/ubuntu/aws_code_pipeline/env/bin/activate
 pip install -r /home/ubuntu/aws_code_pipeline/requirements.txt
